a1d6721b3c575845c1ff3ddf5eecf5eb6ce248e6
Kevin <linlh.lihsuan@gmail.com>
2020-10-17 01:59:33 +0800

    ㄏㄏ

--- app/src/main/java/com/example/bookkeeping/MainClass.java
+++ app/src/main/java/com/example/bookkeeping/MainClass.java
@@ -1,7 +1,10 @@
 package com.example.bookkeeping;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,18 +15,21 @@ import java.util.List;
 public class MainClass extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<Money>ListMoney;
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
         findViews();
-        setData();
+        setIncome_Expenses();
+
         setRecyclerViewAdapter();
         Mydatabase db=new Mydatabase(MainClass.this,Mydatabase._DBName,null,Mydatabase._DBVersion);
 
     }
 
-    private void setData() {
+    private void setIncome_Expenses() {
         ListMoney=new ArrayList<>();
         ListMoney.add(new Money("收入","薪資20K",R.drawable.in));
         ListMoney.add(new Money("支出","冰沙30",R.drawable.out));
@@ -47,10 +53,26 @@ public class MainClass extends AppCompatActivity {
 
     }
 
+
+    // create menu on toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
-
+    }
+    // call menu triger function
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:
+                Toast.makeText(MainClass.this
+                        ,getResources().getString(R.string.menu_about)
+                        ,Toast.LENGTH_SHORT).show();
+            case R.id.create:
+                Toast.makeText(MainClass.this
+                        ,getResources().getString(R.string.menu_create)
+                        ,Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
